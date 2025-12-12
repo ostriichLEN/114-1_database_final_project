64ef16a996d3291b128870d63a5b5d7746645218
ostriichLEN <james205032889@gmail.com>
2025-12-12 20:55:34 +0800

    feat: 整合 ASP.NET Core Identity 符合資安標準會員登入與權限管理系統
    - 安裝Microsoft.AspNetCore.Identity.EntityFrameworkCore 與 UI 套件
    - 建立 AppIdentityContext 並執行 Migration 以生成 Identity 資料表

---
+++ 114-1_database_final_project/Models/AppIdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _114_1_database_final_project.Models
+{
+    // 繼承 IdentityDbContext 以獲得會員資料表的功能
+    public class AppIdentityContext : IdentityDbContext
+    {
+        public AppIdentityContext(DbContextOptions<AppIdentityContext> options)
+            : base(options)
+        {
+        }
+    }
+}
